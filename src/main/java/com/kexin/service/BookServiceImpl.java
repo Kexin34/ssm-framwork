69e94ba0eb158971922e6d727412a1d8bec1479c
Kexin34 <kexinwen.ca@gmail.com>
2021-03-23 23:12:24 -0400

    mybatis layer

---
+++ src/main/java/com/kexin/service/BookServiceImpl.java
@@ -0,0 +1,37 @@
+package com.kexin.service;
+
+import com.kexin.dao.BookMapper;
+import com.kexin.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+
+    // service业务层调用dao层：组合dao
+    private BookMapper bookMapper;
+
+    // 设置一个set接口，方便Spring管理
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
